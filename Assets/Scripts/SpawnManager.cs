f6dd58b84291bdda3b813df677a37b2f431dcce9
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-12-26 10:49:09 +0100

    all good collisionss

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,31 +5,35 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
 
     public float spawnRangeX = 20;
     public float spawnPosZ = 20;
 
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval); //TIMER
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) //"S" is pressed down
-        {
-            Vector3 spawnPosition = new Vector3(
+  
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPosition = new Vector3(
                 Random.Range(-spawnRangeX, spawnRangeX),
                 0, spawnPosZ);
 
-            animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex],
-                spawnPosition,
-                animalPrefabs[animalIndex].transform.rotation);
-        }
-        
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex],
+            spawnPosition,
+            animalPrefabs[animalIndex].transform.rotation);
     }
 }
