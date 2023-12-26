8820ad65e90c81459c8191cbdaa70170d678e6e0
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-12-26 08:23:45 +0100

    boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +15,20 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {//left side
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //right side
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        //Time... is needed to make it update not every frame but every second
         
     }
 }
